53d7ac70ffa2dea9b08b409109685fa4cae44b7e
elbob <72274133+AhmedElsayed111@users.noreply.github.com>
2021-02-17 23:57:12 +0200

    Update Script.lua

--- inc/Script.lua
+++ inc/Script.lua
@@ -2363,6 +2363,7 @@ keyboard.inline_keyboard = {
 },
 }
 local msg_id = msg.id_/2097152/0.5
+https.request("https://lue.telegram.org/bot"..token..'/sendMessage?chat_id=' .. msg.chat_id_ .. '&text=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))		
 end
 
 if MsgText[1] == "روايات" or MsgText[1]=="الروايات" then
